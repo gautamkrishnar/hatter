58d17b2db35099adc75b1c13d4619bcbce692399
zero_coder <aswinrajv@gmail.com>
2014-12-26 10:21:23 +0530

    basic skeleton

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, url_for
+from flask import g, session, request, render_template, flash
+from aiml import Kernel
+from os import listdir
+
+files = listdir('standard')
+
+#CONFIGURATION
+DEBUG = True
+
+
+
+#APP
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+
+bot = Kernel()
+for file in files:
+	bot.learn('standard/'+file)
+
+respon = ' '
+
+@app.before_request
+def before_request():
+	g.respon = 'Hello , I am Tarrant the Hatter. Some people call me the Mad Hatter. I was created by Aswin .He says he was bored one day :D .<br/> A big Hi, by the way'	
+
+@app.route('/',methods=['GET','POST'])
+def index():
+	if request.method == 'POST':
+		reply = bot.respond(request.form['question'])
+		return render_template('index.html',respon=reply)
+	return render_template('index.html',respon=g.respon)
+	
+#running the app
+if __name__ == "__main__":
+	app.run(debug=True)
